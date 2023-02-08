571f382f9d969f705a928474489ade3cd98eddda
mishamisha1 <thanksagain060@gmail.com>
2023-02-08 19:00:32 +0600

    d
    d

---
+++ dz2/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ProductCard v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+
+<script>
+import ProductCard from '../src/components/ProductCard.vue';
+import axios from 'axios';
+
+export default {
+  components: {
+    ProductCard,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  mounted() {
+    axios.get('https://fakestoreapi.com/products')
+      .then(response => {
+        this.products = response.data;
+      });
+  },
+};
+</script>
---
+++ dz2/src/components/ProductCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="product-card"
+    :style="{ transform: expanded ? 'scale(0.5)' : 'scale(1)' }"
+    @mouseenter="expanded = true"
+    @mouseleave="expanded = false"
+  >
+    <img :src="product.image" alt="product image" />
+    <h3>{{ product.title }}</h3>
+    <p>{{ product.price }}</p>
+    <template v-if="expanded">
+      <p>{{ product.description }}</p>
+      <p>SKU: {{ product.sku }}</p>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: Object,
+  },
+  data() {
+    return {
+      expanded: false,
+    };
+  },
+};
+</script>
+
+<style>
+.product-card {
+  width: 1300px;
+  height: 1500px;
+  margin: 16px;
+  overflow: hidden;
+  transition: transform 0.5s ease;
+}
+</style>
